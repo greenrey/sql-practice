7956828da27dc30141a00293e46c6d781887d1ed
greenrey <reyjoo121@gmail.com>
2025-06-30 23:47:59 +0900

    Add solution for leetcode 178 Rank Scores

---
+++ leetcode_178_rank_scores.sql
@@ -0,0 +1,8 @@
+--- 🧩 Leetcode 178. Rank Scores
+--- 🔗 https://leetcode.com/problems/rank-scores/description/
+--- 🎯 Find the rank of the scores using DENSE_RANK
+
+SELECT score
+     , DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
+  FROM Scores;
+ 
