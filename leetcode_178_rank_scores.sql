02c9cb037d17cd778a62375574bdc166f6a71565
greenrey <reyjoo121@gmail.com>
2025-07-02 15:23:03 +0900

    Update leetcode_178_rank_scores.sql

--- leetcode_178_rank_scores.sql
+++ leetcode_178_rank_scores.sql
@@ -1,6 +1,6 @@
---- 🧩 Leetcode 178. Rank Scores
---- 🔗 https://leetcode.com/problems/rank-scores/description/
---- 🎯 Find the rank of the scores using DENSE_RANK
+-- 🧩 Leetcode 178. Rank Scores
+-- 🔗 https://leetcode.com/problems/rank-scores/description/
+-- 🎯 Find the rank of the scores using DENSE_RANK
 
 SELECT score
      , DENSE_RANK() OVER (ORDER BY score DESC) AS 'rank'
